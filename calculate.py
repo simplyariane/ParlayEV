c76e5677eb477d9774481171b168461a26a38c41
orson <orson6322@gmail.com>
2026-01-01 21:58:11 -0500

    initial proj

---
+++ calculate.py
@@ -0,0 +1,10 @@
+import math
+
+def convertOdds(odds):
+    if odds > 0:
+        return 100 / (odds + 100)
+    else:
+        return abs(odds) / (abs(odds) + 100)
+
+def removeVig(over, under):
+    
